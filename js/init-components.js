3db6683a07291424c51f39e9be0b8471cf5c3e25
Luan Gomes <luangm22@gmail.com>
2018-05-18 02:24:03 -0300

    finalizacao estilo tabela e inicio escolha de popup plugin

--- js/init-components.js
+++ js/init-components.js
@@ -6,4 +6,15 @@ $(document).ready(function(){
 
     $('.search-j2').select2();
 
+    $('[data="popup"]').on("click" , function() {
+        console.log("foi");
+    });
+
+    $('[data="popup"]').magnificPopup({
+        items: {
+            src: $('<div class="white-popup">Dynamically created popup</div>'),
+            type: 'inline'
+        }
+    });
+
 });
